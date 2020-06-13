365d0dd16d66f6ab97b29076f1945dd84b3f2553
Izzy Swart <zenerboson@gmail.com>
2020-06-12 21:49:27 -0700

    impl unravel/coalesce for protocolerror

--- src/allocated/mod.rs
+++ src/allocated/mod.rs
@@ -3,6 +3,10 @@ mod functions;
 mod future;
 mod stream;
 mod vec;
+use crate::{
+    future::{FutureExt, MapOk},
+    Coalesce, Unravel,
+};
 use alloc::boxed::Box;
 use core::{
     fmt::{self, Debug, Formatter},
@@ -14,6 +18,33 @@ use thiserror::Error;
 #[derive(Error)]
 #[error(transparent)]
 pub struct ProtocolError(Box<dyn Error + Send>);
+
+impl<C: ?Sized> Unravel<C> for ProtocolError
+where
+    Box<dyn Error + Send>: Unravel<C>,
+{
+    type Finalize = <Box<dyn Error + Send> as Unravel<C>>::Finalize;
+    type Target = <Box<dyn Error + Send> as Unravel<C>>::Target;
+
+    fn unravel(self) -> Self::Target {
+        self.0.unravel()
+    }
+}
+
+impl<C: ?Sized> Coalesce<C> for ProtocolError
+where
+    Box<dyn Error + Send>: Coalesce<C>,
+    <Box<dyn Error + Send> as Coalesce<C>>::Future: Unpin,
+{
+    type Future = MapOk<
+        <Box<dyn Error + Send> as Coalesce<C>>::Future,
+        fn(Box<dyn Error + Send>) -> ProtocolError,
+    >;
+
+    fn coalesce() -> Self::Future {
+        Box::<dyn Error + Send>::coalesce().map_ok(ProtocolError)
+    }
+}
 
 impl ProtocolError {
     pub fn new<T: Error + Send + 'static>(error: T) -> Self {
